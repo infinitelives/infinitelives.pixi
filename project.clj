d52bcbcaa215cc07e6ab9940909d758a54eeeb32
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-01 10:57:39 +0800

    project file

---
+++ project.clj
@@ -0,0 +1,24 @@
+(defproject infinitelives.pixi "0.1.0-SNAPSHOT"
+  :description "2D pixi library for cljs games"
+  :url "https://github.com/infinitelives"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "0.0-3308"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+
+  :plugins [[lein-cljsbuild "1.0.6"]]
+
+  :source-paths ["src/cljs"]
+  :test-paths ["test"]
+
+  :cljsbuild
+  {
+   :builds {:test
+            {:source-paths ["src" "test"]
+             :compiler {:output-to "resources/test/compiled.js"
+                        :optimizations :whitespace
+                        :pretty-print true}}}
+   :test-commands {"test" ["phantomjs"
+                           "resources/test/test.js"
+                           "resources/test/test.html"]}})
