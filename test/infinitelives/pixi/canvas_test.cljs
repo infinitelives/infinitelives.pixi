5d68242f92ea8ab097476fd2ffb9f805210a6cf3
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-01 14:24:25 +0800

    test making canvas. and cleanup func.

--- test/infinitelives/pixi/canvas_test.cljs
+++ test/infinitelives/pixi/canvas_test.cljs
@@ -1,7 +1,24 @@
 (ns infinitelives.pixi.canvas-test
   (:require [cljs.test :refer-macros [deftest is]]
-            [infinitelives.pixi.canvas :as canvas]))
+            [infinitelives.pixi.canvas :as canvas]
+            [goog.dom :as dom]))
+
+(defn- nodelist->seq
+  "returns a lazy-seq of all the nodes in the nodelist"
+  ([nodelist]
+   (nodelist->seq nodelist 0))
+  ([nodelist i]
+   (when (< i (.-length nodelist))
+     (cons (aget nodelist i)
+           (lazy-seq (nodelist->seq nodelist (inc i)))))))
+
+(defn- remove-all-dom-canvas
+  "remove every canvas from the DOM"
+  []
+  (doseq [node (nodelist->seq (dom/$$ "canvas"))]
+    (dom/removeNode node)))
 
 (deftest make
-  (let [c (canvas/make :engine :canvas
-                       :canvas (.getElementById js/document "app"))]))
+  (is (canvas/make :engine :canvas))
+  (is (canvas/make :engine :auto))
+  (remove-all-dom-canvas))
