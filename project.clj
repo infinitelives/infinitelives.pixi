cb17924827f2c06bd4f9f950bb589f4ba006278a
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-01 12:48:18 +0800

    should we require dommy?

--- project.clj
+++ project.clj
@@ -5,7 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3308"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+
+                 ;; should we depend on this? maybe its better without?
+                 [prismatic/dommy "1.1.0"]
+                 ]
 
   :plugins [[lein-cljsbuild "1.0.6"]]
 
