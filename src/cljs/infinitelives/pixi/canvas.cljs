0876340d68c9f1e0aae650468c92c7f70944df5a
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-16 11:22:26 +0800

    get some basic dom handling stuff from dommy. This should be replaced with goog.dom calls.

--- src/cljs/infinitelives/pixi/canvas.cljs
+++ src/cljs/infinitelives/pixi/canvas.cljs
@@ -11,6 +11,30 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
 )
 
+(defn as-str
+  "Coerces strings and keywords to strings, while preserving namespace of
+   namespaced keywords"
+  [s]
+  (if (keyword? s)
+    (str (some-> (namespace s) (str "/")) (name s))
+    s))
+
+(defn set-style!
+  "Set the style of `elem` using key-value pairs:
+      (set-style! elem :display \"block\" :color \"red\")"
+  [elem & kvs]
+  (assert (even? (count kvs)))
+  (let [style (.-style elem)]
+    (doseq [[k v] (partition 2 kvs)]
+      (.setProperty style (as-str k) v))
+    elem))
+
+(defn append!
+  "Append `child` to `parent`"
+  ([parent child]
+     (doto parent
+       (.appendChild child))))
+
 (defn make
   "make a new pixi canvas, or initialise pixi with an existing canvas.
 
