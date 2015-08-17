2999d55ca9b6204a1489f5c510beb51d1fb2893c
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-17 21:14:47 +0800

    add sprite alpha option

--- src/cljs/infinitelives/pixi/sprite.cljs
+++ src/cljs/infinitelives/pixi/sprite.cljs
@@ -10,12 +10,19 @@
 
 (defn make-sprite
   "construct a sprite by its texture. optionally pass in other things"
-  [texture & {:keys [x y xhandle yhandle]
-              :or {x 0 y 0 xhandle 0.5 yhandle 0.5}}]
+  [texture & {:keys [x y xhandle yhandle scale alpha]
+              :or {x 0 y 0
+                   xhandle 0.5 yhandle 0.5
+                   scale [1 1]
+                   alpha 1}}]
   (let [s (PIXI/Sprite. texture)]
     (set! (.-anchor s) (make-point xhandle yhandle))
     (set! (.-x s) x)
     (set! (.-y s) y)
+    (when-not (= scale [1 1])
+      (set! (.-scale s) (make-point (get scale 0) (get scale 1))))
+    (when-not (= 1 alpha)
+      (set! (.-alpha s) alpha))
     s))
 
 (defn set-pos!
