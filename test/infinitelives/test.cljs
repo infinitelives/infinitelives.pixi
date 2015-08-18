fe1b2fec3964687514d92ca2a70fefb37ce61d87
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-18 20:32:34 +0800

    parentheses

--- test/infinitelives/test.cljs
+++ test/infinitelives/test.cljs
@@ -2,7 +2,7 @@
   (:require [cljs.test :refer-macros [run-all-tests]]
             [infinitelives.pixi.canvas-test]
             [infinitelives.pixi.resources-test]
-            [infinitelives.pixi.texture-test])
+            [infinitelives.pixi.texture-test]))
 
 (enable-console-print!)
 
