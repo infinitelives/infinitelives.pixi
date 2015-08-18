5cacbd3d609381cef21d192784add6af582ce42e
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-18 20:30:09 +0800

    texture test

--- test/infinitelives/test.cljs
+++ test/infinitelives/test.cljs
@@ -1,7 +1,8 @@
 (ns infinitelives.test
   (:require [cljs.test :refer-macros [run-all-tests]]
             [infinitelives.pixi.canvas-test]
-            [infinitelives.pixi.resources-test])
+            [infinitelives.pixi.resources-test]
+            [infinitelives.pixi.texture-test])
 
 (enable-console-print!)
 
