64b313a59d77c454b4f69a72edc8ab9ed7d511a1
Crispin Wellington <retrogradeorbit@gmail.com>
2016-01-03 15:37:49 +0800

    texture object cache

--- src/cljs/infinitelives/pixi/texture.cljs
+++ src/cljs/infinitelives/pixi/texture.cljs
@@ -3,3 +3,26 @@
 
 (defn sub-texture [texture [x y] [w h]]
   (PIXI/Texture. texture (PIXI/Rectangle. x y w h)))
+
+(defonce texture-store (atom {}))
+
+(defn add!
+  [key texture]
+  (swap! texture-store assoc key texture)
+  )
+
+(defn remove!
+  ([key]
+   (swap! texture-store dissoc key)))
+
+(defn get
+  [key]
+  (@texture-store key))
+
+(defn load-sprite-sheet!
+  [texture asset-description]
+  (swap! texture-store
+         #(into %
+                (for [[key {:keys [pos size]}] asset-description]
+                  [key
+                   (sub-texture texture pos size)]))))
