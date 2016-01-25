bdb09b25eef1cc182dc62c7ca8bb98dd0badd94a
Crispin Wellington <retrogradeorbit@gmail.com>
2016-01-25 21:43:42 +0800

    code to build char location definitions from image

--- src/clj/infinitelives/pixi/pixelfont.clj
+++ src/clj/infinitelives/pixi/pixelfont.clj
@@ -35,16 +35,40 @@
 ;;   [& args]
 ;;   (println "Hello, World!"))
 
+(defn strips [strip-fn dim image x1 x2 y1 y2]
+  (let [strip-sizes
+        (-> image
+            (strip-fn x1 x2 y1 y2)
+            (->> (map-indexed vector)
+                 (partition-by second)
+                 (map count))
+            )]
+    ;strip-sizes
+    (partition 2 (for [n (range (count strip-sizes))]
+                   (apply + (case dim :y y1 :x x1) (take (inc n) strip-sizes))))))
+
+(def horizontal-strips (partial strips transparent-hlines :y))
+(def vertical-strips (partial strips transparent-vlines :x))
+
+(defn char-dimensions [image xi1 xi2 yi1 yi2 chars]
+  (let [hstrips (horizontal-strips image xi1 xi2 yi1 yi2)
+        vsize (apply max (map #(Math/abs (apply - %)) hstrips))]
+    (map
+     conj
+     (for [[y1 y2] hstrips
+           [x1 x2] (vertical-strips image xi1 xi2 y1 y2)]
+       [x1 x2 y1 (+ y1 vsize)])
+     chars)))
+
+
+
 (comment
   (def a (-> "test.png"
              io/file
              ImageIO/read
-             (transparent-hlines 0 200 0 200)
-             (->> (map-indexed vector)
-                  (partition-by second)
-                  (map count))
+                                        ;(horizontal-strips 0 200 0 200)
+                                        ;(process-line 0 1000 89 96)
+             (char-dimensions 127 350 84 128 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+    )
 
-             ))
-
-
-  (for [n (range (count a))] (apply + (take (inc n) a))))
+  )
