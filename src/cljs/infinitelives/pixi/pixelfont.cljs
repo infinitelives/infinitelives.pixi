85d79d07e23e35faa7b8d10f8aedb956972b0ae2
Crispin Wellington <retrogradeorbit@gmail.com>
2016-02-03 19:58:23 +0800

    just some logging hooks for now

--- src/cljs/infinitelives/pixi/pixelfont.cljs
+++ src/cljs/infinitelives/pixi/pixelfont.cljs
@@ -1,6 +1,15 @@
-(ns infinitelives.pixi.pixelfont)
+(ns infinitelives.pixi.pixelfont
+  (:require [infinitelives.pixi.texture :as t]
+            [infinitelives.pixi.resources :as r]))
 
 (defn load-pixel-font
   [pixel-font-name texture-key layout]
+  (.log js/console (str pixel-font-name))
+  (.log js/console (str texture-key))
+  (.log js/console (str layout))
+  )
+
+(defn make-text [font-key text]
+  (.log js/console (str font-key " => " text))
 
 )
