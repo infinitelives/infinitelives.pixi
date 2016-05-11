a03ff800f627df065f9dc9b66c5c951fe7864cb1
Crispin Wellington <retrogradeorbit@gmail.com>
2016-05-12 00:27:16 +0800

    tiling sprite options

--- src/cljs/infinitelives/pixi/sprite.cljs
+++ src/cljs/infinitelives/pixi/sprite.cljs
@@ -13,13 +13,19 @@
 (defn make-sprite
   "construct a sprite by its texture. optionally pass in other things"
   [texture & {:keys [x y xhandle yhandle scale alpha interactive mousedown
-                     rotation tint]
+                     rotation tint tiling tiling-width tiling-height]
               :or {x 0 y 0
                    xhandle 0.5 yhandle 0.5
                    scale 1
                    alpha 1
-                   rotation 0}}]
-  (let [s (js/PIXI.Sprite. (if (keyword? texture) (texture/get texture) texture))]
+                   rotation 0
+                   tiling false}}]
+  (let [s (if tiling
+            (js/PIXI.extras.TilingSprite.
+             (if (keyword? texture) (texture/get texture) texture)
+             tiling-width tiling-height)
+            (js/PIXI.Sprite. (if (keyword? texture) (texture/get texture) texture)))]
+    (assert s "creation of sprite failed and returned nil")
     (set! (.-anchor s) (make-point xhandle yhandle))
     (set! (.-x s) x)
     (set! (.-y s) y)
