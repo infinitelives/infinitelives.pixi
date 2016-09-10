5942659a61104ce2cd9b895a6690c7c7d45942c6
Crispin Wellington <retrogradeorbit@gmail.com>
2016-09-10 23:52:52 +0800

    start text layout functions

--- src/cljs/infinitelives/pixi/pixelfont.cljs
+++ src/cljs/infinitelives/pixi/pixelfont.cljs
@@ -68,13 +68,13 @@
               (recur l (+ xp w 1.0 koff) yp c (conj sprite-set sprite))
               (conj sprite-set sprite))))))))
 
-(defn add-text! [batch font-key text]
-  (doseq [spr (make-char-sprite-set font-key text)]
+(defn add-text! [batch font-key text tint]
+  (doseq [spr (make-char-sprite-set font-key text tint)]
     (.addChild batch spr)))
 
-(defn change-text! [batch font-key text]
+(defn change-text! [batch font-key text tint]
   (clear-text! batch)
-  (add-text! batch font-key text))
+  (add-text! batch font-key text tint))
 
 (defn make-text [font-key text & {:keys [tint scale anchor rotation
                                          x y visible
@@ -122,6 +122,75 @@
         (<! (timeout delay))
         (when r (recur r))))
     batch))
+
+(defn string-width
+  "return the width of this text in this font (in unit pixels)."
+  [font-key text]
+  (let [{:keys [font kerning space]} (get-font font-key)]
+    (+
+     ;; char sizes
+     (reduce + (for [c text]
+                 (if (= " " c)
+                   space
+                   ((:size (font c)) 0))))
+
+     ;; kerning adjustments
+     (reduce + (map (comp kerning str) text (subs text 1)))
+
+     ;; char spacing
+     (dec (count text)))))
+
+(defn char-fits? [font-key x left right char]
+  (let [{:keys [font kerning space]} (get-font font-key)
+        {:keys [size]} (font char)]
+    (if (< (+ x size)) (+ x size) left)))
+
+(defn regex-modifiers
+  "Returns the modifiers of a regex, concatenated as a string."
+  [re]
+  (str (if (.-multiline re) "m")
+       (if (.-ignoreCase re) "i")))
+
+(defn re-pos
+  "Returns a vector of vectors, each subvector containing in order:
+   the position of the match, the matched string, and any groups
+   extracted from the match."
+  [re s]
+  (let [re (js/RegExp. (.-source re) (str "g" (regex-modifiers re)))]
+    (loop [res []]
+      (if-let [m (.exec re s)]
+        (recur (conj res (vec (cons (.-index m) m))))
+        res))))
+
+
+(defn word-beginnings-ends
+  "given a string of words:
+
+  'this is a string of words'
+
+  return the slice position of the end of each word as a vector
+
+  [4 7 9 16 19 25]
+  "
+  [s]
+  (map
+   (fn [[n t]] [n (+ n (count t))])
+   (re-pos #"\S+" s)))
+
+(defn how-many-words-fit [font-key text ends size]
+  (loop [n 0
+         [e & r] ends]
+    (let [words (subs text 0 e)
+          width (string-width font-key words)
+          ]
+      (log "words" words width r n size
+           )
+      (if (and (< width size) r)
+        (recur (inc n) r)
+        n))))
+
+
+
 
 (comment
   (infinitelives.pixi.pixelfont/load-pixel-font :test-font :test [["A" 146 89 140 97] ["B" 154 89 148 97]])
