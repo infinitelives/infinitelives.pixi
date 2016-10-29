1fb6ee48d6f173222a3509a8014cce04a03d2b08
Crispin Wellington <retrogradeorbit@gmail.com>
2016-10-29 21:40:06 +0800

    include il.pixi

--- examples/tilemap/project.clj
+++ examples/tilemap/project.clj
@@ -9,7 +9,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
                  [org.clojure/core.async "0.2.391"
-                  :exclusions [org.clojure/tools.reader]]]
+                  :exclusions [org.clojure/tools.reader]]
+
+                 [infinitelives/infinitelives.pixi "0.1.1-SNAPSHOT"]]
 
   :plugins [[lein-figwheel "0.5.8"]
             [lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]]
