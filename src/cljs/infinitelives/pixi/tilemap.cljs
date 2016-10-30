afc1c21fadbbc5caebed86d70617a76ef5fd9d89
Crispin Wellington <retrogradeorbit@gmail.com>
2016-10-30 20:32:12 +0800

    rejig tilemap api to support tile changing

--- src/cljs/infinitelives/pixi/tilemap.cljs
+++ src/cljs/infinitelives/pixi/tilemap.cljs
@@ -22,21 +22,28 @@
 (defn make-tile-sprites [{texture-map :texture-map
                           [tile-width tile-height] :tile-size}
                          tile-map]
-  (filter identity
-          (for [row (range (count tile-map))
-                col (range (count (first tile-map)))]
-            (let [char (nth (tile-map row) col)]
-              (when char
-                (s/make-sprite (texture-map char)
-                               :x (* tile-width col) :y (* tile-height row)
-                               :xhandle 0 :yhandle 0))))))
+  (into
+   {}
+   (filter identity
+           (for [row (range (count tile-map))
+                 col (range (count (first tile-map)))]
+             (let [char (nth (tile-map row) col)]
+               (when char [[col row]
+                           (s/make-sprite (texture-map char)
+                                          :x (* tile-width col) :y (* tile-height row)
+                                          :xhandle 0 :yhandle 0)]))))))
 
-(defn make-tilemap [tile-set tile-mapping tile-map-chars & opts]
+(defn make-tile-map [tile-mapping tile-map-chars]
+  (strs->keymap tile-mapping tile-map-chars))
+
+(defn make-tilemap [sprite-set & opts]
   (apply s/make-container
-   :children (make-tile-sprites
-              tile-set
-              (strs->keymap tile-mapping tile-map-chars))
-   :xhandle 0.5 :yhandle 0.5
-   :particle true
-   opts)
-)
+         :children (vals sprite-set)
+         :xhandle 0.5 :yhandle 0.5
+         :particle true
+         opts))
+
+(defn alter-tile! [sprite-set coord tile-set texture-key]
+  (set!
+   (.-texture (sprite-set coord))
+   (-> tile-set :texture-map texture-key)))
